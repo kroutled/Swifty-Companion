1889e4318584603f10922fd6e6aa368ea3936d35
Kyle ROUTLEDGE <kroutled@e4r7p8.wethinkcode.co.za>
2018-10-29 11:04:39 +0200

    added api stuff

--- SwiftyCompanion/SwiftyCompanion/ViewController.swift
+++ SwiftyCompanion/SwiftyCompanion/ViewController.swift
@@ -8,8 +8,10 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+var userName: String = ""
+var token = ""
 
+class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -19,7 +21,25 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    func getUserInfo() {
+        var request = URLRequest(url: URL(string: "https://api.intra.42.fr/v2/users/"+(userName)+"?access_token="+(token))!)
+        request.httpMethod = "GET"
+        let dispatch = DispatchGroup()
+        let task = URLSession.shared.dataTask(with: request as URLRequest) {
+            (data, response, error) in
+            //print(response as Any)
+            if error != nil {
+                print(error as Any)
+            }
+            else if let d = data {
+                json1 = JSON(d)
+            }
+            dispatch.leave()
+        }
+        dispatch.enter()
+        task.resume()
+        dispatch.wait()
+    }
 }
 
